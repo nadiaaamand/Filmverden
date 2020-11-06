4d606cad00d82b34a81a6b9c918999cef6197652
nadiaaamand <nadiaaamand@gmail.com>
2020-11-06 10:27:52 +0100

    Tilføjet kommentar til et nyt indlæg (som ikke skal bruges, men så er det safe))

---
+++ blog_indlaeg4.php
@@ -0,0 +1,220 @@
+<!doctype html>
+<html lang="da">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">	
+<meta charset="UTF-8">
+<!--Bootstrap stylesheet-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	
+	<!--Stylesheet-->
+	<link rel="stylesheet" href="css/style.css">
+	
+	<!--Bootstrap JS-->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+	
+	<!-- Font awesome icons-->
+	<script src="https://kit.fontawesome.com/336a1c920c.js" crossorigin="anonymous"></script>
+	
+</head>
+
+<body>
+ <?php 
+	include 'nav.php';
+?>
+		<div class="container">
+	<div class="row">
+		<div class="col-sm-12 col-lg-6 offset-lg-3">
+	<!--Kilde: https://www.dfi.dk/viden-om-film/filmdatabasen/film/captain-america-first-avenger-->
+		<h1>CAPTAIN AMERICA: THE FIRST AVENGER</h1>
+<p>Joe Johnston, USA, 2011 124 min.U/Spillefilm</p>
+
+<p>Hans mod og hans vilje kan der ikke være nogen tvivl om, men den spinkle Steve Rogers bliver kasseret, da han melder sig frivilligt til hæren i 1941. Snart finder militæret imidlertid andet at bruge ham til. Den geniale Dr. Erskine er undsluppet fra nazisterne, og han har udviklet et eksperimentalt serum, der kan styrke et mands naturlige anlæg og forvandle ham til en supersoldat. Således forvandles Rogers til Captain America. Men inden Erskine undslap, var han blevet tvunget til at foretage et lignende eksperiment i Tyskland, på en langt mindre sympatisk kandidat.</p>
+			
+	<!--Kilde: pixabay-->
+	<img class="img-fluid mb-2" src="img/ca.png" alt="ca">
+
+		<h2>Grundoplysninger</h2> 
+<p>Original titel: Captain America: The First Avenger</p>
+
+<p>Dansk titel: Captain America: The First Avenger</p>
+
+<p>Instruktør: Joe Johnston</p>
+
+<p>Manuskript: Christopher Markus, Stephen McFeely</p>
+
+<p>Producer: Kevin Feige, Amir Madani</p>
+
+<p>Fotograf: Shelly Johnson</p>
+
+<p>Klipper: Robert Dalva, Jeffrey Ford</p>
+
+<p>Komponist: Alan Menken, Alan Silvestri</p>
+
+<p>Scenograf: Rick Heinrichs</p>
+
+<p>Medvirkende: Chris Evans, Hayley Atwell, Sebastian Stan</p>
+
+<p>Produktionsland: USA</p>
+
+<p>Produktionsselskab: Marvel Enterprises</p>
+
+<p>Dansk distribution: United International Pictures</p>
+
+<p>Dansk biografpremiere: 11.08.2011</p>
+
+<p>Premierebiografer: Palads, Imperial, Albertslund Biograferne, Gladsaxe Bio, Værløse Bio, BioCity (Tåstrup), Kastrup Bio, Birkerød Bio, Baltoppen Bio 1-2, CinemaxX, Empire, Kinopalæet (Lyngby), Falkoner samt 49 biografer i provinsen, inkl. Færøerne. I både 3D og 35 mm</p>
+
+<p>Aldersgrænse: Tilladt for børn fra 11 år </p>
+
+<!--Kilde: https://www.youtube.com/watch?v=8P-3KCeXpys&ab_channel=JoBloSuperheroes-->
+<iframe width="560" height="315" src="https://www.youtube.com/embed/8P-3KCeXpys" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		
+		<h2>Credits</h2> 
+<h4>Selskaber</h4>
+<p>Produktionsselskab: Marvel Enterprises</p>
+<p>Dansk distribution: United International Pictures</p>
+
+
+<h4>Instruktion</h4>
+<p>Instruktion: Joe Johnston</p>
+
+<h4>Manus</h4>
+<p>Manus: Christopher Markus og Stephen McFeely</p>
+
+<h4>Produktion</h4>
+<p>Producer: Kevin Feige og Amir Madani</p>
+
+<h4>Foto</h4>
+<p>Foto: Shelly Johnson</p>
+
+<h4>Stills</h4>
+<p>Stills: Jay Maidment</p>
+
+<h4>Klip</h4>
+<p>Klip: Robert Dalva og Jeffrey Ford</p>
+
+<h4>Musik</h4>
+<p>Musik: Alan Menken og Alan Silvestri</p>
+
+<h4>Produktionsdesign</h4>
+<p>Produktionsdesign: Rick Heinrichs</p>
+
+<h4>Medvirkende</h4>
+<p>Captain America/Steve Rogers: Chris Evans</p>
+<p>Peggy Carter: Hayley Atwell</p>
+<p>James Buchanan 'Bucky' Barnes: Sebastian Stan</p>	
+			
+<p>Kilde: Det Danske Filminstitut </p> 			
+			</div>
+		
+</div>
+			
+<hr>
+	
+	<!-- Media object -> Om forfatteren-->		
+<div class="container p-4 m-4">
+	<div class="row">
+	<div class="col-lg-8 offset-2">
+	<div class="media">
+  <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" class="align-self-start mr-3 rounded-circle" alt="..." width="150">
+  <div class="media-body">
+	  <div class="justify-content-between d-flex">
+    <h5 class="mt-0">Om forfatteren</h5>
+	  <div>
+	  <a class="mr-3"><i class="fab fa-facebook-f"></i></a>
+		<a class="mr-3"><i class="fab fa-instagram"></i></a>
+		  </div>
+		  </div>
+    <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+  </div>
+</div>
+	</div>
+	</div>
+	</div>
+			
+	<!--Kommentar-->
+						<hr>
+			
+	<!--Kommentar-->
+			<div class="col-lg-6 offset-lg-3 p-4 mb-4">
+			<!-- if user is not signed in, tell them to sign in. If signed in, present them with comment form -->
+			<?php if (isset($iduser)): ?>
+				<form class="clearfix" action="post_details.php" method="post" id="comment_form">
+					<textarea name="comment_text" id="comment_text" class="form-control"></textarea>
+					<button class="btn btn-primary btn-sm pull-right" id="submit_comment">Submit comment</button>
+				</form>
+			<?php else: ?>
+				<div class="border-light">
+					<h4 class="text-center pb-4"><a href="login.php">Login</a> for at kommentere på indlægget</h4>
+				</div>
+			<?php endif ?>
+			<!-- Display total number of comments on this post  -->
+			<h5><span id="comments_count"><?php echo count($comments) ?></span> Kommentar(er)</h5>
+			<hr>
+			<!-- comments wrapper -->
+			<div id="comments-wrapper">
+			<?php if (isset($comments)): ?>
+				<!-- Display comments -->
+				<?php foreach ($comments as $comment): ?>
+				<!-- comment -->
+				<div class="comment clearfix">
+					<img src="<?php $img ?>" alt="" class="profile_pic">
+					<div class="comment-details">
+						<span class="comment-name"><?php echo getUsernameById($comment['iduser']) ?></span>
+						<span class="comment-date"><?php echo date("F j, Y ", strtotime($comment["created_at"])); ?></span>
+						<p><?php echo $comment['body']; ?></p>
+						<a class="reply-btn" href="#" data-id="<?php echo $comment['id']; ?>">Kommenter</a>
+					</div>
+					<!-- reply form -->
+					<form action="post_details.php" class="reply_form clearfix" id="comment_reply_form_<?php echo $comment['id'] ?>" data-id="<?php echo $comment['id']; ?>">
+						<textarea class="form-control" name="reply_text" id="reply_text"></textarea>
+						<button class="btn btn-primary btn-xs pull-right submit-reply">Send kommantar</button>
+					</form>
+
+					<!-- GET ALL REPLIES -->
+					<?php $replies = getRepliesByCommentId($comment['id']) ?>
+					<div class="replies_wrapper_<?php echo $comment['id']; ?>">
+						<?php if (isset($replies)): ?>
+							<?php foreach ($replies as $reply): ?>
+								<!-- reply -->
+								<div class="comment reply clearfix">
+									<img src="<?php $img ?>" alt="" class="profile_pic">
+									<div class="comment-details">
+										<span class="comment-name"><?php echo getUsernameById($reply['iduser']) ?></span>
+										<span class="comment-date"><?php echo date("F j, Y ", strtotime($reply["created_at"])); ?></span>
+										<p><?php echo $reply['body']; ?></p>
+										<a class="reply-btn" href="#">Send kommentar</a>
+									</div>
+								</div>
+							<?php endforeach ?>
+						<?php endif ?>
+					</div>
+				</div>
+					<!-- // comment -->
+				<?php endforeach ?>
+			<?php else: ?>
+				<h6>Vær den første til at kommentere på indlægget</h6>
+			<?php endif ?>
+			</div><!-- comments wrapper -->
+		</div><!-- // all comments -->
+		
+		</div>
+	
+	
+	
+	
+ <?php 
+	require 'footer.php';
+?>
+		<!-- Javascripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<!-- Bootstrap Javascript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="scripts.js"></script>
+</body>
+</html>
