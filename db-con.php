c6cfa209b16af5d73d71d5196cdd4f0c132b06e4
nadiaaamand <nadiaaamand@gmail.com>
2020-12-20 21:49:02 +0100

    Update db-con.php

--- db-con.php
+++ db-con.php
@@ -1,5 +1,6 @@
 <?php 
-$conn = mysqli_connect("localhost", "root", "root", "filmverden");
+
+$conn = mysqli_connect("http://lamp2.sdu.dk/~naaam19", "naaam19", "23081993", "naaam19_db");
 
 if (!$conn) {
 	die("Connection failed: ". mysqli_connect_error());
